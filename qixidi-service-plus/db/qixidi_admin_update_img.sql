f068cbd593b16b4ace2882380f153ef3618e805c
ziwei <2978824265@qq.com>
2025-02-20 16:02:33 +0800

    perf(db文件): 保存更新sql

--- qixidi-service-plus/db/qixidi_admin_update_img.sql
+++ qixidi-service-plus/db/qixidi_admin_update_img.sql
@@ -2,30 +2,29 @@
 
 -- 文章表
 UPDATE b_article_information
-SET article_cover = REPLACE ( article_cover, 'http://116.198.203.132:9300/qixidi', 'https://minio.qixidi.top/qixidi' ),
-    article_content = REPLACE ( article_content, 'http://116.198.203.132:9300/qixidi', 'https://minio.qixidi.top/qixidi' ),
-    article_content_md = REPLACE ( article_content_md, 'http://116.198.203.132:9300/qixidi', 'https://minio.qixidi.top/qixidi' );
+SET article_cover = REPLACE ( article_cover, 'https://zsh-aurora.oss-cn-beijing.aliyuncs.com', 'https://minio.qixidi.top/qixidi' ),
+    article_content = REPLACE ( article_content, 'https://zsh-aurora.oss-cn-beijing.aliyuncs.com', 'https://minio.qixidi.top/qixidi' ),
+    article_content_md = REPLACE ( article_content_md, 'https://zsh-aurora.oss-cn-beijing.aliyuncs.com', 'https://minio.qixidi.top/qixidi' );
 
 --专辑表
 UPDATE b_dictum_album
-SET cover = REPLACE ( cover, 'http://116.198.203.132:9300/qixidi', 'https://minio.qixidi.top/qixidi' );
+SET cover = REPLACE ( cover, 'https://zsh-aurora.oss-cn-beijing.aliyuncs.com', 'https://minio.qixidi.top/qixidi' );
 
 -- 用户表
 UPDATE b_user_main
-SET avatar = REPLACE ( avatar, 'http://116.198.203.132:9300/qixidi', 'https://minio.qixidi.top/qixidi' );
+SET avatar = REPLACE ( avatar, 'https://zsh-aurora.oss-cn-beijing.aliyuncs.com', 'https://minio.qixidi.top/qixidi' );
 
 -- 名言分组
 UPDATE b_dictum_group
-SET cover = REPLACE ( cover, 'http://116.198.203.132:9300/qixidi', 'https://minio.qixidi.top/qixidi' );
+SET cover = REPLACE ( cover, 'https://zsh-aurora.oss-cn-beijing.aliyuncs.com', 'https://minio.qixidi.top/qixidi' );
 
 
 -- 名言信息表
 UPDATE b_dictum_info
-SET content = REPLACE ( content, 'http://116.198.203.132:9300/qixidi', 'https://minio.qixidi.top/qixidi' ),
-    content_md = REPLACE ( content_md, 'http://116.198.203.132:9300/qixidi', 'https://minio.qixidi.top/qixidi' );
-
+SET content = REPLACE ( content, 'https://zsh-aurora.oss-cn-beijing.aliyuncs.com', 'https://minio.qixidi.top/qixidi' ),
+    content_md = REPLACE ( content_md, 'https://zsh-aurora.oss-cn-beijing.aliyuncs.com', 'https://minio.qixidi.top/qixidi' );
 
 
 -- 专栏表
 UPDATE b_special_information
-SET cover = REPLACE ( cover, 'http://116.198.203.132:9300/qixidi', 'https://minio.qixidi.top/qixidi' );
+SET cover = REPLACE ( cover, 'https://zsh-aurora.oss-cn-beijing.aliyuncs.com', 'https://minio.qixidi.top/qixidi' );
