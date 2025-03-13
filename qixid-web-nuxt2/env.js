c0da7e801a0cb870f64c1affd7fe3aba2da4e750
ziwei <2978824265@qq.com>
2025-03-13 18:34:51 +0800

    perf: 页面整体优化

--- qixid-web-nuxt2/env.js
+++ qixid-web-nuxt2/env.js
@@ -9,8 +9,7 @@ const config = {
   dev: {
     NODE_ENV: 'dev',
     // 代理的后端请求服务器地址
-    // SERVER_URL: '127.0.0.1:9001',
-    SERVER_URL: '116.198.203.132:9001',
+    SERVER_URL: '127.0.0.1:9001',
     SERVICE_PROTOCOL: 'http://',
     WEBSOCKET_PROTOCOL: 'ws://',
     THEME_COLOR: defaultConfig.THEME_COLOR,
@@ -28,7 +27,7 @@ const config = {
   prod: {
     NODE_ENV: 'prod',
     // 代理的后端请求服务器地址
-    SERVER_URL: 'server.qixidi.top',
+    SERVER_URL: '127.0.0.1:9001',
     SERVICE_PROTOCOL: 'https://',
     WEBSOCKET_PROTOCOL: 'wss://',
     THEME_COLOR: defaultConfig.THEME_COLOR,
