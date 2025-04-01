cf19c9dd79d6ffcb0ec6d5571a4540c9b871c03d
ziwei <2978824265@qq.com>
2025-04-01 15:46:39 +0800

    perf: ui/交互优化

--- qixid-web-nuxt2/components/article/article-search-list.vue
+++ qixid-web-nuxt2/components/article/article-search-list.vue
@@ -21,9 +21,9 @@
           <el-row :gutter="20" class="mb-10">
             <nuxt-link :to="`/article/article-details/`+$base64.encode(item.id)" target="_blank">
               <el-col :span=" item.articleCover==null?24:20" class="cursor-pointer" title="查看文章">
-                <div class="font-bold-s line-height-24 font-s-16 mb-10 text-underline-hover">
-                  {{ item.articleTitle}}
-                </div>
+                <h1 class="line-height-24 font-s-16 mb-10 text-underline-hover font-bold-s">
+                  {{ item.articleTitle }}
+                </h1>
                 <div class="font-s-14 line-height-26 abstract-cl-2 font-bold-300">
                   {{ item.articleAbstract }}
                 </div>
