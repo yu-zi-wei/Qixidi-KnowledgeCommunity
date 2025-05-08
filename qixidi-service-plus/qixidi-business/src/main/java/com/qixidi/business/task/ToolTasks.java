643cf2bbb18c865b7a2be9aed1b1125837b64b0c
ziwei <2978824265@qq.com>
2025-05-08 10:19:44 +0800

    perf: 优化

--- qixidi-service-plus/qixidi-business/src/main/java/com/qixidi/business/task/ToolTasks.java
+++ qixidi-service-plus/qixidi-business/src/main/java/com/qixidi/business/task/ToolTasks.java
@@ -32,7 +32,7 @@ import java.util.Objects;
 public class ToolTasks {
     public static final Logger logger = LoggerFactory.getLogger(ToolTasks.class);
 
-    @Scheduled(cron = "0 0 2,5 * * ?")//每天凌晨2点、5点执行
+    @Scheduled(cron = "0 0 6,8 * * ?")//每天凌晨5点、7点执行
 //    @Scheduled(cron = "0 */2 * * * ?")
     public void jueJin() throws IOException, MessagingException {
         logger.info("掘金签到开始");
