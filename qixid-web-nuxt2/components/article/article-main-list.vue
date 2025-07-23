7795a20c6be60ff3ba3f7bb15fd879e9f20a67ab
ziwei <2978824265@qq.com>
2025-07-23 17:19:12 +0800

    perf: 优化首页触底拉取多次接口问题

--- qixid-web-nuxt2/components/article/article-main-list.vue
+++ qixid-web-nuxt2/components/article/article-main-list.vue
@@ -131,7 +131,6 @@ export default {
       let scrollHeight = document.documentElement.scrollHeight
       if ((scrollTop + clientHeight + clientHeight) >= scrollHeight) {
         if (!this.scrollLoading) return;
-        console.log("触底")
         this.load()
       }
     },
